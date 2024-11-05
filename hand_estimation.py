fbd25e8e892436e1418d13855fd985eaacb3124a
Jobjorn <672641@stud.hvl.no>
2024-11-05 15:34:45 +0100

    fix

--- hand_estimation.py
+++ hand_estimation.py
@@ -90,7 +90,7 @@ def main():
         ctime=0
         ptime=0
         cap = cv2.VideoCapture(0)
-        detector = HandTrackingDynamic()
+        detector = HandEstimation()
         cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
         cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
         if not cap.isOpened():
