8c0e540947968ca305ed1a14d82300b6c3884c51
Sondre <sondrelangedal.ness@gmail.com>
2024-11-13 16:12:12 +0100

    commit

--- components/navbar.py
+++ components/navbar.py
@@ -2,10 +2,11 @@
 from streamlit_option_menu import option_menu
 
 def display_navbar():
+
     selected = option_menu(
         menu_title=None,
-        options=["Home", "Recipes", "Settings", "About"],  # Added "Recipes"
-        icons=["house", "book", "gear", "info-circle"],  # Added an icon for "Recipes"
+        options=["Home", "Recipes", "About"],  # Added "Recipes"
+        icons=["house", "book", "info-circle"],  # Added an icon for "Recipes"
         menu_icon="cast",
         default_index=0,
         orientation="horizontal",
